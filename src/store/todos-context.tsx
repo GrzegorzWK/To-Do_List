45da43daef33b2f946e9810ee0d6662b422c6952
GrzegorzWK <grzegorzwalesiak@gmail.com>
2023-07-02 16:50:49 +0200

    context

--- src/store/todos-context.tsx
+++ src/store/todos-context.tsx
@@ -1,5 +1,35 @@
-const ToDosContextProvider = () => {
-  return;
+import React, { useState } from "react";
+import ToDo from "../models/todo";
+
+type ToDosContextObj = {
+  item: ToDo[];
+  addToDo: (text: string, deadline: string) => void;
+  removeToDo: (id: string) => void;
+};
+
+export const ToDosContext = React.createContext<ToDosContextObj>({
+  item: [],
+  addToDo: () => {},
+  removeToDo: (id: string) => {},
+});
+
+const ToDosContextProvider: React.FC = (props) => {
+  const [toDos, setToDos] = useState<ToDo[]>([]);
+
+  const addToDoHandler = () => {};
+  const removerToDoHandler = () => {};
+
+  const contextValue: ToDosContextObj = {
+    item: toDos,
+    addToDo: addToDoHandler,
+    removeToDo: removerToDoHandler,
+  };
+
+  return (
+    <ToDosContext.Provider value={contextValue}>
+      {props.children}
+    </ToDosContext.Provider>
+  );
 };
 
 export default ToDosContextProvider;
